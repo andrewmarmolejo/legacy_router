3da5de40b9f94f116f564ceebaa8f1d6c83c446b
andrewmarmolejo <35593189+andrewmarmolejo@users.noreply.github.com>
2020-04-19 22:55:15 -0700

    Added comments & edited spacing

--- legacy_router.py
+++ legacy_router.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # File: legacy_router.py
 
+###     IMPORTS
 from mininet.net import Mininet
 from mininet.node import Host, Node
 from mininet.cli import CLI
@@ -14,13 +15,18 @@ def myNetwork():
 
     info( '*** Adding controller\n' )
     info( '*** Add switches\n')
+    
+    #   CREATES GLOBAL HOST r1
     r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    #   IP FORWARDING
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
+    #   ADDS HOSTS TO INTERACT
     info( '*** Add hosts\n')
     h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
     h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-
+    
+    #   ADDS LINKS TO h1 & h2
     info( '*** Add links\n')
     net.addLink(h1, r1)
     net.addLink(h2, r1)
