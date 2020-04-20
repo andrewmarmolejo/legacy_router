413379fbbb7de75d77adf85879d4dcbdbfcc5e9a
andrewmarmolejo <35593189+andrewmarmolejo@users.noreply.github.com>
2020-04-19 23:07:30 -0700

    ADDED IMPORTS

--- legacy_router.py
+++ legacy_router.py
@@ -6,7 +6,14 @@ from mininet.net import Mininet
 from mininet.node import Host, Node
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost
+from mininet.nodelib import LinuxBridge #OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.link import TCLink, Intf
+from subprocess import call
 
+###     NETWORK FUNCTION
 def myNetwork():
     
     net = Mininet( topo=None,
